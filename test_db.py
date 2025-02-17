413b70ef355c376b6252001ce4ad1128a4efc802
Frederik Klinkby Jacobsen <frederik0303@hotmail.com>
2025-02-17 15:04:43 +0100

    Adding vector embedding

--- test_db.py
+++ test_db.py
@@ -1,14 +1,16 @@
 from astrapy import DataAPIClient
 import os
 from dotenv import load_dotenv
+import openai
 
 # Load environment variables from .env file
 load_dotenv()
 
-# Get the value of 'db-token'
+# Get db-token and OpenAI key
 db_token = os.getenv("db_token")
-# Initialize the client
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# Initialize the client
 client = DataAPIClient(db_token)
 db = client.get_database_by_api_endpoint(
   "https://69bdc51f-37ed-492f-8423-1c7f943410ec-westus3.apps.astra.datastax.com"
@@ -17,18 +19,31 @@ db = client.get_database_by_api_endpoint(
 print(f"Connected to Astra DB: {db.list_collection_names()}")
 
 
-## Query collection
+# Query collection
 collection = db.get_collection("fcm_rag_collection")
-id = "79c12811634c4ee5a8f492c259ddb9f4" #change to see other chunks
-query = {"_id": id}
 
-###make dictionary of all id's to be able to access content with indexes instead of typing actual id string
+# Query to fetch only '_id' fields
+documents = collection.find({}, projection={"_id": 1})  # Only fetch '_id' field
 
-# Find document
-document = collection.find_one(query)
+# Generate list of all ids
+ids = []
+for doc in documents:
+    ids.append(doc["_id"])
 
-# Extract and print `page_content`
-if document:
-    print(document.get("page_content"))
+""" To find page_content of single ids
+doc = collection.find_one({'_id': ids[0]}) # Change to find other docs
+
+if doc:
+  print("Page content:", doc["page_content"])
+
 else:
-    print("Document not found")
+  print("Page content not found")
+
+ """
+
+###If a text string can be converted to a vector embedding, it is perhaps possible to perform similarity searches in the db:
+query_text = "Hvad koster et sæsonkort?"
+response = openai.embeddings.create(model='text-embedding-3-large', input=query_text)
+query_embedding = response.data[0].embedding
+
+print(query_embedding)
