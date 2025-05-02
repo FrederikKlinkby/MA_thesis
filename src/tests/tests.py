322a5b606887de85da1d7a6ed1b4efc1e16aaccc
Frederik Klinkby Jacobsen <frederik0303@hotmail.com>
2025-05-02 14:44:57 +0200

    Create function to test bot

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -1,45 +1,20 @@
-from langchain_openai import ChatOpenAI
 from dotenv import find_dotenv, load_dotenv
 import os
-from ragas.dataset_schema import SingleTurnSample 
-from ragas.metrics import Faithfulness
-import asyncio
-from ragas.integrations.langchain import LangchainLLMWrapper
-from ragas.llms import llm_factory
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import retrieval.indexing as indexing
+import chatbot.chatbot as bot
+
 
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
-async def test():
-    # Create the base LLM
-    base_llm = ChatOpenAI(
-        model="gpt-4o-mini",
-        temperature=0.0,
-        api_key=OPENAI_API_KEY
-    )
-    
-    # Define evaluator llm
-    evaluator_llm = LangchainLLMWrapper(langchain_llm=base_llm)
-    scorer = Faithfulness(llm=evaluator_llm)
+def test():
+    question = 'Hvem er næste kamp imod?'
+    splits = indexing.split_data(chunk_size=1000, chunk_overlap=150)
+    vectorstore = indexing.store_data(splits)
+    bot.chatbot(vectorstore, question, 0.1, 5, 'mmr', OPENAI_API_KEY, show_retrieved=True)
 
-    #Translate prompts into Danish
-    danish_prompts = await scorer.adapt_prompts(language='danish', llm=evaluator_llm)
-    scorer.set_prompts(**danish_prompts)
-    
-    sample = SingleTurnSample(
-        user_input="Hvornår starter billetsalget til næste kamp?",
-        response="Billetsalget starter typisk 14 dage før kampen",
-        retrieved_contexts=[
-            "Billetsalget til FC Midtjyllands hjemmebanekampe åbner som regel 14 dage før kampene. Du kan se den specifikke åbningsdato for billetsalget på www.fcm.dk."
-        ]
-    )
-    
-
-    #score = await scorer.single_turn_ascore(sample)
-    #print(f"Faithfulness score: {score}")
-    print(danish_prompts)
-    return #score
-
-asyncio.run(test())
+test()
